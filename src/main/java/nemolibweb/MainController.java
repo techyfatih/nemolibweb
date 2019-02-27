bd85232cc7d4efd16a7f119edeef0760000db04a
Fatih Ridha <abgfatih@gmail.com>
2019-02-26 19:40:08 -0800

    hello there

---
+++ src/main/java/nemolibweb/MainController.java
@@ -0,0 +1,13 @@
+package nemolibweb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello there";
+    }
+}
